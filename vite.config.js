b878cf615cb3344cdd0bb43407852bce311851d8
lucianoescuderopalma <airaoinvasor055@gmail.com>
2025-10-15 00:19:45 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist', // o 'build', dependiendo de lo que quieras subir
+    outDir: 'dist', 
   },
-  base: './', // importante para que Amplify pueda servir los archivos con rutas relativas
+  base: './', 
 });
